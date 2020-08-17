45a450e37ee2d662292d1a5872c2649cd437bfac
Anya Brun <tsbrun@icloud.com>
2020-08-17 18:31:25 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,14 @@
 class Meal
+  attr_reader :waiter, :customer, :total, :tip 
+
+  @@all = []
+
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter 
+    @customer = customer
+    @total = total 
+    @tip = tip 
+    @@all << self 
+  end
 
 end
